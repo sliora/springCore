90b6bc8b434fb24d260c7bf6a8a10bb666db84c2
zerom-H <cupdisin@gmail.com>
2022-02-22 23:10:01 +0900

    Test code Add

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given 주어질때
+        Member member = new Member(1L, "MemberA", Grade.VIP);
+
+        //when 이렇게 했을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then 이렇게 된다.
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
