00bc5d4e142d86cd12d8c5fad0cea2e8cd5f1e10
zerom-H <cupdisin@gmail.com>
2022-02-27 01:14:24 +0900

    DI

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,10 +1,22 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
+//객체의 생성과 연결은 AppConfig가 담당
+//관심사 분리
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl();
+        //memoryMemberRepository 객체를 생성하고 그 참조값을 MemberServiceImpl을 생성하면서 생성자로 전달
+        //클라이언트인 MemberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입하는 것 같다고해서 DI(Dependency Injection) 의존성 주입
+        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,13 +7,19 @@ import hello.core.member.*;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private MemberRepository memberRepository;
     //DIP 위반 (추상화에 의존해야 하는데 구체에 의존)
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     //DIP를 지키면..구현체가 없다.
     private DiscountPolicy discountPolicy;
+
+    //생성자를 만듬으로써 DIP를 지킨다.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
