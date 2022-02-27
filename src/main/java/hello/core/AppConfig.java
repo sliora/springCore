abfd3f4f23dd3294c086dcea452ed966cbdd5587
zerom-H <cupdisin@gmail.com>
2022-02-27 23:52:25 +0900

    스프링 컨테이너로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,10 +9,14 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //객체의 생성과 연결은 AppConfig가 담당
 //관심사 분리
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         //memoryMemberRepository 객체를 생성하고 그 참조값을 MemberServiceImpl을 생성하면서 생성자로 전달
         //클라이언트인 MemberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입하는 것 같다고해서 DI(Dependency Injection) 의존성 주입
@@ -20,14 +24,17 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository()); //option + cmmand + m 을 눌러서 메소드를 추출함
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //순수 자바로 DI
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+
+        //스프링프레임워크를 활용하여 DI
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
