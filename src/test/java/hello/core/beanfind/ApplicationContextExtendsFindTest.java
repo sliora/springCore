c9f4d4654fb8192563ae382c9013d232f8ae7500
zerom-H <cupdisin@gmail.com>
2022-03-04 00:00:39 +0900

    스프링 빈 조회 - 상속 관계 테스트 추가

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -40,6 +42,26 @@ public class ApplicationContextExtendsFindTest {
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+
+        for(String key : beansOfType.keySet()){
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - 오브젝트")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for(String key : beansOfType.keySet()){
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
 
 
     @Configuration
