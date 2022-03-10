51a394216d0424c429a4379e43ab515311d7d7fb
zerom-H <cupdisin@gmail.com>
2022-03-11 01:50:32 +0900

    싱글톤 무상태 테스트

--- src/test/java/hello/core/singletonTest/StatefulService.java
+++ src/test/java/hello/core/singletonTest/StatefulService.java
@@ -1,14 +1,23 @@
 package hello.core.singletonTest;
 
 public class StatefulService {
-    private int price; //상태를 유지하는 필드
-
+    //아래 주석 부분과 같이 구현하면 상태 유지가 됨 조심!
+/*    private int price; //상태를 유지하는 필드
     public void order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
         this.price = price; // 여기가 문제!
     }
-
-    public int getPrice() {
+        public int getPrice() {
         return price;
     }
+
+    */
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+
+
+
+
 }
